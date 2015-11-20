3fe05d3394f1a12b1bdf7b2a7ecc2c53ad47e6de
Doug Hill <quickstep25@me.com>
2015-11-20 09:10:47 -0500

    added demo ing to master

--- frontend/src/js/components/template/template.js
+++ frontend/src/js/components/template/template.js
@@ -1,29 +1,34 @@
 /*global define, console*/
-define(["knockout", "text!./template.html"], function (ko, templateHTML) {
+define(["knockout", "text!./template.html", "google.analytics"], function (ko, templateHTML, ga) {
 	"use strict";
 
 	// DEFINE VIEW MODEL
 	var TemplateViewModel = function (params) {
 		this.params			= ko.observable(params);
 		this.componentID	= ko.observable('component-page-' + params.page);
+		this.pageview();
 	};
 
 	// ADD ACTION FUNCTIONALITY TO THE INTERFACE / VIEWMODEL
-
 	//  TemplateViewModel.prototype.doSomething = function() {
 	//    this.message('You invoked doSomething() on the viewmodel.');
 	//  };
 
 	// EXTEND FUNCTIONS ON THE VIEW MODEL AFTER INTITIALIZATION
+	ko.utils.extend(TemplateViewModel.prototype, {
 
-	//	TemplateViewModel.prototype.extend({
-	//		someFunction: function () {
-	//			...do some actions here
-	//		}
-	//	});
+		// SEND PAGE VIEW TO GOOGLE
+		pageview : function () {
+			ga('create', 'UA-26957181-12', 'auto');
+			ga('set', {
+				page: 'index',
+				title: 'Web Application Template'
+			});
+			ga('send', 'pageview');
+		}
+	});
 
     //  HOUSEKEEPING DISPOSE AND CLEANUP
-
     //	TemplateViewModel.prototype.dispose = function () { };
 
 	return { viewModel: TemplateViewModel, template: templateHTML };
